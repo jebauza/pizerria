74cbfd4fa85ccd30444a949685ca87ca97c3b4a3
jebauza <jebauza@gmail.com>
2019-09-30 23:14:49 +0200

    final 4

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('pizza','PizzaController');
+Route::apiResource('pizzas','PizzaController');
+Route::apiResource('ingredientes','IngredienteController');
--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,6 @@
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return redirect('feed');
+    return view('welcome');
 });
-
-//Route::resource('feed', 'FeedController');
 //Route::resource('pizza', 'PizzaController');
-
-/*Route::get('/prueba', function() {
-
-    $crawler = Goutte::request('GET', 'https://www.elpais.com/');
-    $crawler->filter('#bloque_actualidad_destacadas .bloque__interior article.articulo')->each(function ($node,$i=0) {
-      
-      if($i<5)
-      {
-        $arr = [] ;
-        $link = $node->filter('h2.articulo-titulo a');
-        $arr['title'] = $link->text();
-        $url = $link->attr('href');
-        $arr['autor'] = $node->filter('span.autor-nombre a')->text();
-        $arr['image'] = $node->filter('figure.foto img')->attr('src');
-
-        dump($arr);
-      }
-      $i++;
-    });
-});
